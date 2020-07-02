38f58f67ee3b90a718948137b4ce8ccf95ad491d
Niklas Schlögel <niklas.schloegel@student.hs-rm.de>
2020-07-02 11:46:38 +0200

    Aufzählungstyp für AutoModelle implementiert

--- src/main/java/streetsim/ui/spielfeld/elemente/AutoView.java
+++ src/main/java/streetsim/ui/spielfeld/elemente/AutoView.java
@@ -8,7 +8,6 @@ public class AutoView extends ImageView {
 
     public AutoView(Image image){
         super();
-        ResourceAssist assist = ResourceAssist.getInstance();
         this.setImage(image);
     }
 }
