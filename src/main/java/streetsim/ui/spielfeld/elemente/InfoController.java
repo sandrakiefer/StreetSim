f7ee8d1440b56fa7f9889f5b85b1eacd666f6b16
Niklas Schlögel <niklas.schloegel@student.hs-rm.de>
2020-07-09 18:51:08 +0200

    Startseiten Bugfix, JavaDoc

--- src/main/java/streetsim/ui/spielfeld/elemente/InfoController.java
+++ src/main/java/streetsim/ui/spielfeld/elemente/InfoController.java
@@ -10,12 +10,22 @@ import streetsim.ui.AbstractController;
 import streetsim.ui.StreetSimApp;
 import streetsim.ui.utils.PopUpAssist;
 
+/**
+ * Eine Controller-Klasse, die benutzt werden soll, wenn der Benutzer
+ * Fehleingaben während der Laufzeit macht.
+ * Diese Klasse zeigt dem Benutzer ein kleines PopUp-Fenster, dass ihn über
+ * seine Fehleingabe informiert
+ */
 public class InfoController extends AbstractController<StreetSimApp> {
 
     private Label nachricht;
     private Stage popup;
     private PopUpAssist popAssist;
 
+    /**
+     * Konstruktor des Controllers
+     * @param app Hauptanwendung
+     */
     public InfoController(StreetSimApp app) {
         super(app);
         rootView = new InfoView();
@@ -25,11 +35,18 @@ public class InfoController extends AbstractController<StreetSimApp> {
         handlerAnmelden();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void handlerAnmelden() {
 
     }
 
+    /**
+     * Diese Methode erstellt und zeigt ein PopUp Fenster.
+     * @param info Warnungstext, der angezeigt wird
+     */
     public void zeige(String info) {
         System.out.println(info);
         nachricht.setText(info);
